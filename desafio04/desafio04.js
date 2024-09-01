1cee75da99d8c2779433b57b9074f4f1e3f93bfa
silvakwan1 <kauans003@gmail.com>
2024-09-01 16:20:11 -0300

    apaguei o desafio.md

--- desafio04/desafio04.js
+++ desafio04/desafio04.js
@@ -1,13 +1,3 @@
-// console.log("opi");
-// "4. Dado o valor de faturamento mensal de uma distribuidora, detalhado por estado:
-//    • SP – R$67.836,43
-//    • RJ – R$36.678,66
-//    • MG – R$29.229,88
-//    • ES – R$27.165,48
-//    • Outros – R$19.849,53
-
-// Escreva um programa na linguagem que desejar onde calcule o percentual de representação que cada estado teve dentro do valor total mensal da distribuidora.
-// "
 const faturamentoPorEstado = {
   SP: 67836.43,
   RJ: 36678.66,
@@ -30,7 +20,4 @@ for (let chave in faturamentoPorEstado) {
   faturamentoPorEstadoPorcentagem[chave] = formate.replace(".", ",") + "%";
 }
 
-console.log(faturamentoPorEstadoPorcentagem);
 console.table(faturamentoPorEstadoPorcentagem);
-
-console.log(somaTotal);
